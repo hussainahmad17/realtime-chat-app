3b63d8e3147b0e396c4b583362f1b093e8cc1bb4
hussainahmad <hussainahmad.dev.17@gmail.com>
2025-06-08 16:30:55 +0500

    edit index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,15 +29,22 @@ app.use(
   })
 );
 
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src * 'self' blob: data:;");
+  next();
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, '../dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
 }
 
 server.listen(PORT, () => {
